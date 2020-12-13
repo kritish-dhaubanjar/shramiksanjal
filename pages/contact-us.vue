d3cd55e455f6d3c0d7b628a7f0ca9fc822e0710b
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2020-12-13 17:15:47 +0545

    minor color swaps

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,10 +1,10 @@
 <template>
-  <section>
+  <section class="bg-light">
     <Banner />
     <div class="container-fluid py-5 my-5">
       <div class="row">
         <div class="col-md-6 col-lg-4">
-          <div class="card is-radiusless my-3 bg-light is-borderless">
+          <div class="card is-radiusless my-3 bg-white is-borderless shadow-sm">
             <div class="card-body py-5">
               <h4 class="mb-3">Main Branch</h4>
               DE 198 Tech Road<br />
@@ -14,7 +14,7 @@
           </div>
         </div>
         <div class="col-md-6 col-lg-4">
-          <div class="card is-radiusless my-3 bg-light is-borderless">
+          <div class="card is-radiusless my-3 bg-white is-borderless shadow-sm">
             <div class="card-body py-5">
               <h4 class="mb-3">Quick Contact</h4>
               <i class="las la-phone-alt mr-2" />+05 9875 654 32<br />
@@ -23,9 +23,19 @@
             </div>
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="card is-radiusless my-3 form-card">
+        <!-- <div class="col-md-6 col-lg-4">
+          <div class="card is-radiusless my-3 bg-light is-borderless">
             <div class="card-body py-5">
+              <h4 class="mb-3">Quick Contact</h4>
+              <i class="las la-phone-alt mr-2" />+05 9875 654 32<br />
+              <i class="las la-fax mr-2" />123 456 7890<br />
+              <i class="las la-envelope mr-2" />info@example.com<br />
+            </div>
+          </div>
+        </div> -->
+        <div class="col-lg-4">
+          <div class="card is-radiusless my-3 is-borderless">
+            <div class="card-body py-5 form-card bg-white shadow-sm">
               <h5 class="font-weight-bold mb-3">
                 Need more information or want to get in touch?
               </h5>
@@ -34,7 +44,7 @@
                 follow up with you in 1-2 business days.
               </p>
 
-              <form class="my-4 pt-3">
+              <form class="mt-4 pt-3">
                 <div class="form-group">
                   <input
                     type="text"
@@ -90,7 +100,7 @@
       </div>
     </div>
 
-    <section class="bg-light py-5">
+    <section class="py-5 bg-white">
       <div class="container-fluid">
         <div class="row mt-5">
           <div class="col-lg-8 mb-5">
@@ -158,17 +168,29 @@ h3 {
 
 input {
   height: 48px;
-  border: 1px solid #e3e3e3;
+  border: 2px solid #e3e3e3;
+  &:active,
+  &:focus {
+    border-width: 2px;
+  }
 }
 
 textarea {
   resize: none;
-  border: 1px solid #e3e3e3;
+  border: 2px solid #e3e3e3;
+  &:active,
+  &:focus {
+    border-width: 2px;
+  }
 }
 
 @media screen and(min-width: 992px ) {
   .form-card {
     position: absolute;
   }
+}
+
+.form-card.card-body {
+  border: 1px solid #e3e3e3;
 }
 </style>
