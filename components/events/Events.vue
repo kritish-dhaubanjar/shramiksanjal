67b9e4dd4661606ccf46291446fd0723d2e8946d
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2020-12-16 21:49:50 +0545

    events/event added

---
+++ components/events/Events.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class="bg-light">
+    <h5 class="font-weight-bold mb-5 bg-light pt-5 px-3">
+      Upcoming Events & Meetings
+    </h5>
+
+    <div
+      class="card is-radiusless is-borderless mb-1 bg-light"
+      v-for="i in 5"
+      :key="i"
+    >
+      <div class="card-body">
+        <div class="row">
+          <!-- <div
+            class="col-sm align-items-center d-none d-sm-flex align-items-center"
+          >
+            <div>
+              <div class="background-image rounded-circle" />
+            </div>
+          </div> -->
+          <div class="col-sm-auto">
+            <span
+              class="badge badge-dark is-radiusless px-2 font-weight-light py-1"
+            >
+              Aug 12 - Aug 13
+            </span>
+
+            <small class="ml-3">
+              9:00 am - 9:00 pm
+            </small>
+            <h5 class="mt-3 font-weight-bold">Along Pines Hike</h5>
+            <p class="mb-0">
+              Hands-on activities, student photography exhibit, and more. All
+              activities are free and Museum admission fee is not required.
+            </p>
+          </div>
+          <div class="col-sm d-flex align-items-center">
+            <button
+              class="btn btn-danger is-radiusless font-weight-bold px-4 py-2 my-3"
+            >
+              DETAILS
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr class="mx-3" />
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.background-image {
+  background-image: url(https://via.placeholder.com/128x128);
+  height: 100px;
+  width: 100px;
+}
+
+.badge {
+  font-size: 12px;
+}
+.lead {
+  font-size: 16px !important;
+}
+
+.row {
+  .col-sm:nth-child(1) {
+    max-width: 128px;
+  }
+
+  .col-sm:nth-child(3) {
+    max-width: 140px;
+  }
+}
+
+@media screen and(min-width: 576px) {
+  .col-sm:nth-child(3) {
+    border-left: 1px solid #e3e3e3;
+  }
+}
+
+.pattern {
+  height: 300px;
+  width: 100%;
+  background-image: url("/images/assets/dot.png");
+}
+
+.card:last-child {
+  hr {
+    display: none;
+  }
+}
+</style>
---
+++ pages/events/event.vue
@@ -0,0 +1,122 @@
+<template>
+  <section class="mb-5 pb-5">
+    <Jumbotron />
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-8 mt-3">
+          <main class="mb-5">
+            <div class="card bg-light is-radiusless is-borderless mb-4 pt-5">
+              <div class="card-body">
+                <h5>
+                  Hands-on activities, student photography exhibit, and more.
+                  All activities are free and Museum admission fee is not
+                  required.
+                </h5>
+
+                <hr class="my-5" />
+
+                <div class="row my-4">
+                  <div class="col-lg-6 d-flex">
+                    <i class="las la-clock la-3x mr-3" />
+                    <div>
+                      <em>Event Date:</em>
+                      <p><b>Sep 11</b> at <b>9:00 am</b> - <b>9:00 pm</b></p>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-6 d-flex">
+                    <i class="las la-map-marker la-3x mr-3" />
+                    <div>
+                      <em>Location:</em>
+                      <p>
+                        <b>Macky Auditorium Concert Hall,</b> 1595 Pleasant St,
+                        Boulder
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--  -->
+            <div class="content">
+              <p>
+                Presented by the Frisco Heritage Association and the Heritage
+                Museum. Hands-on activities, student photography exhibit, and
+                more. All activities are free and Museum admission fee is not
+                required. Call 972-292-5668 for additional information.
+              </p>
+
+              <p><b>Schedule</b></p>
+
+              <p><b>9:30</b> –morning refreshments</p>
+
+              <p>
+                <b>10 a.m.</b>– Keith Volanto, PhD, Collin College professor of
+                history and author of Texas, Cotton and the New Deal.
+              </p>
+
+              <p>
+                <b>11 a.m. to 2 p.m.</b>— Community experts display and
+                demonstrate contemporary cotton crafts in the 2nd floor
+                galleries.
+              </p>
+
+              <ul>
+                <li>Visit with the Frisco Quilt Guild</li>
+                <li>
+                  Watch and learn from a certified Master Weaver about creating
+                  cloth from thread on a loom
+                </li>
+                <li>Sarees from India display</li>
+                <li>Latin American textiles display</li>
+              </ul>
+
+              <p>
+                <b>Noon</b> — Images of Frisco, student photography exhibit and
+                reception, sponsored by Collin College.
+              </p>
+            </div>
+          </main>
+        </div>
+
+        <div class="col-lg-4 mt-3">
+          <Events />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Jumbotron from "@/components/news/Jumbotron";
+import Events from "@/components/events/Events";
+
+export default {
+  components: {
+    Jumbotron,
+    Events
+  }
+};
+</script>
+
+<style scoped lang="scss">
+p {
+  line-height: 190%;
+  margin-bottom: 24px;
+}
+ul {
+  margin-top: 16px;
+  margin-bottom: 16px;
+  li {
+    padding-top: 4px;
+    padding-bottom: 4px;
+  }
+}
+@media screen and(min-width: 992px) {
+  main,
+  .col-lg-4 {
+    transform: translateY(-64px);
+  }
+}
+</style>
