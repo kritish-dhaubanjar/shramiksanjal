8d5eaf2dbf9b5e2776cdfb9add8fadc498fe4fa9
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-01-28 20:19:47 +0545

    Section Dynamic

--- components/index/Section.vue
+++ components/index/Section.vue
@@ -1,33 +1,58 @@
 <template>
-  <section class="py-5 background-image">
+  <section
+    class="py-5 background-image"
+    :style="
+      `background-image: url(https://api.shramiksanjal.org/${data.image.path})`
+    "
+  >
     <div class="container-fluid my-5 pb-5">
       <div class="col-lg-6 offset-lg-6 text-white">
         <div class="my-5 py-5">
           <h1 class="no-letter-spacing font-weight-bold">
-            The housing crisis is the most serious issue.
+            {{ $localeContent(data, "title", $i18n.locale) }}
           </h1>
           <hr />
           <p class="font-weight-bold">
-            Issue that is making every other challenge harder, particularly for
-            low-income families. This critical effort holds great promise for
-            finding local and regional solutions.
+            {{ $localeContent(data, "overview", $i18n.locale) }}
           </p>
 
-          <button
+          <nuxt-link
+            to="/our-programs"
             class="btn btn-dark btn-lg is-radiusless mr-3 font-weight-bold p-3 mb-2"
           >
-            FIND SOLUTION
-          </button>
-          <button
+            OUR PROGRAMS
+          </nuxt-link>
+          <nuxt-link
+            to="/blogs"
             class="btn btn-dark btn-lg is-radiusless font-weight-bold p-3 mb-2"
           >
-            TOOLS TO HELP ENGAGE
-          </button>
+            RECENT BLOGS
+          </nuxt-link>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {
+        image: {
+          path: ""
+        }
+      }
+    };
+  },
+
+  beforeCreate() {
+    this.$axios.get("/api/singletons/get/banner").then(({ data }) => {
+      this.data = data;
+    });
+  }
+};
+</script>
 
 <style scoped lang="scss">
 @import "@/assets/scss/colors.scss";
