51689fb5751da44d07f2194cb6f08d272b45525d
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-07-14 20:39:20 +0545

    Update FAB.vue

--- components/about/FAB.vue
+++ components/about/FAB.vue
@@ -2,7 +2,7 @@
   <a href="https://forum.shramiksanjal.org/" target="_blank">
     <div class="d-flex align-items-center bg-info text-white py-2 px-3">
       <i class="las la-globe-asia la-2x"></i>
-      <h5 class="mb-0">Visit our forum</h5>
+      <h5 class="mb-0">Meet our all team members</h5>
     </div>
   </a>
 </template>
